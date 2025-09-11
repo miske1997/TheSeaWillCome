d3dca9ca11ba912940628777ada7f54a00d2b4d8
milos stoiljkovic <miskestoiljkovic@gmail.com>
2025-09-11 17:55:28 +0200

    build ui added

--- Scripts/Globals/GameData.gd
+++ Scripts/Globals/GameData.gd
@@ -5,6 +5,12 @@ var currentLevel: Node : set = _set_current_level
 var saveData: SaveData
 var settings
 var controllerConected := false
+var castleResources: Dictionary[String, ResourceData] : get = _get_resources
+var _resourceData: Resources
+
+func _get_resources():
+	return _resourceData.resources
+
 
 func _set_current_level(value: Node):
 	Events.onCurrentLevelChanged.emit(value)
